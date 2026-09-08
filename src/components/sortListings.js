a3452420af5f30c5399bcd29af3fc952ae30de5b
ND-Nitro <steffenheltne@hotmail.com>
2026-09-08 19:31:32 +0200

    adding listing and sorting functionality nr2

--- src/components/sortListings.js
+++ src/components/sortListings.js
@@ -1,4 +1,5 @@
-export function renderSortListings() {
+// this function will render the sort dropdown menu and make the function work  after choise
+export function renderSortListings(onSortChange) {
   const sortElement = globalThis.document?.querySelector("#sort-listings");
 
   if (!sortElement) return;
@@ -19,7 +20,7 @@ export function renderSortListings() {
 
     <select 
     id="listing-sort"
-    class="cursor-pointer rounded-full border gborder-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-700 outline-none transition hover:border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-100">
+    class="cursor-pointer rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-700 outline-none transition hover:border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-100">
 
     ${sortOptions
       .map(
@@ -33,4 +34,9 @@ export function renderSortListings() {
       </select>
     </div>
   `;
+  const sortSelect = sortElement.querySelector("#listing-sort");
+
+  sortSelect?.addEventListener("change", (event) => {
+    onSortChange(event.target.value);
+  });
 }
